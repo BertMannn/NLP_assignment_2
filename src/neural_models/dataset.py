d8e0df0123e4675adc18d29990f00395c5a329e2
Bertran <bertran.vd.giessen@hotmail.com>
2026-03-05 12:30:10 +0100

    wrapped data in pytorch dataset

---
+++ src/neural_models/dataset.py
@@ -0,0 +1,24 @@
+import torch
+from torch.utils.data import Dataset
+
+class AGNewsDataset(Dataset):
+    """PyTorch Dataset for AG News using preprocessed token IDs"""
+
+    def __init__(self, texts, labels):
+        """
+        texts: list of lists of token IDs
+        labels: list of integers
+        """
+        self.texts = texts
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        # convert to torch tensors
+        x = torch.tensor(self.texts[idx], dtype=torch.long)
+        y = torch.tensor(self.labels[idx], dtype=torch.long)
+        return x, y
+    
+    
